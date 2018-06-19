316f839b90cfd40bdf9e179db48ad001c4d974e3
Lewis Sharpe <lewis.sharpe12@googlemail.com>
2018-06-19 11:19:54 +0100

    'Initial-commit'

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express"),
+      app = express(),
+      hbs = require("hbs"),
+      fs = require("fs");
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log("Unable to append to server.log.");
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    pageTitle: "Home",
+    welcomeMessage: "Hello and welcome to my site"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    pageTitle: 'About Page'
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
